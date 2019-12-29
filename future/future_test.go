c2afa24a977c5c8c5cc3f3b941642c331ce91d14
Alex Shch <xantax@ukr.net>
2019-12-29 18:07:39 +0200

    increase test coverage of future

--- future/future_test.go
+++ future/future_test.go
@@ -1,8 +1,11 @@
 package future
 
-import "testing"
+import (
+	"context"
+	"testing"
+)
 
-func Test_NewFuture(t *testing.T) {
+func Test_New(t *testing.T) {
 	t.Run("future should wait until callable returns the result", func(t *testing.T) {
 		var externalVar bool
 
@@ -15,6 +18,29 @@ func Test_NewFuture(t *testing.T) {
 
 		if externalVar != true {
 			t.Error("eternal variable was expected to be modified")
+		}
+	})
+}
+
+func Test_NewWithContext(t *testing.T) {
+	t.Run("future should wait until callable returns the result", func(t *testing.T) {
+		var externalVar bool
+
+		callable := func() error {
+			externalVar = true
+			return nil
+		}
+
+		ctx, cancel := context.WithCancel(context.Background())
+		cancel()
+
+		err := NewWithContext(ctx, callable)()
+		if err.Error() != "context canceled" {
+			t.Error(`"context canceled" error was expected`)
+		}
+
+		if externalVar != false {
+			t.Error("external variable should not be modified")
 		}
 	})
 }
