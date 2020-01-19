32ffa84eb61a7a3d64dae6426d7adea412a26308
Alex Shch <xantax@ukr.net>
2020-01-19 20:08:30 +0200

    minor fixes

--- sync/wait_group.go
+++ sync/wait_group.go
@@ -3,14 +3,11 @@ package sync
 import "sync"
 
 // WaitGroup is a wrapper over sync.WaitGroup providing additional functionality
-type WaitGroup struct{ *sync.WaitGroup }
-
-// NewWaitGroup creates new WaitGroup
-func NewWaitGroup() *WaitGroup { return &WaitGroup{&sync.WaitGroup{}} }
+type WaitGroup struct{ sync.WaitGroup }
 
 // Run adds provided functions to the group (incrementing the group counter)
 // running each callable in a separate goroutine
-func (w WaitGroup) Run(fns ...func()) {
+func (w *WaitGroup) Run(fns ...func()) {
 	for _, fn := range fns {
 		w.Add(1)
 		go func(callable func()) { callable(); w.Done() }(fn)
--- sync/wait_group_test.go
+++ sync/wait_group_test.go
@@ -6,7 +6,7 @@ const total = 10
 
 func Test_WaitGroup(t *testing.T) {
 	t.Run("given a WaitGroup", func(t *testing.T) {
-		wg := NewWaitGroup()
+		var wg WaitGroup
 
 		t.Run("should wait until all functions exit", func(t *testing.T) {
 			resultChan := make(chan struct{})
